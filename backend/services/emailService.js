4c56032d47b3ee32122e8c947a43958da6621b05
jan <jan_yrin_anonuevo@dlsl.edu.ph>
2025-10-23 15:22:07 +0800

    typo

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -24,7 +24,7 @@ class EmailService {
       console.log('📧 Using Gmail service for:', process.env.EMAIL_USER);
 
       try {
-        this.transporter = nodemailer.createTransporter({
+        this.transporter = nodemailer.createTransport({
           service: 'gmail',
           auth: {
             user: process.env.EMAIL_USER,
